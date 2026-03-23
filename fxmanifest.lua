9b72e3e2d007a5ee6c140ba1d745c3b8ffc05994
acwDevs <acw36541@gmail.com>
2026-03-22 23:13:23 -0700

    Prepared for portal upload.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 author 'Auzzie'
 description 'Auzzie Notification System'
 version '1.0.0'
+lua54 'yes'
 
 shared_scripts {
     'shared/config.lua',
@@ -22,4 +23,10 @@ files {
     'nui/index.html',
     'nui/assets/notify.js',
     'nui/assets/notify.css',
+}
+
+escrow_ignore {
+    'shared/config.lua',
+    'client/client.lua',
+    'server/server.lua',
 }
